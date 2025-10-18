b95d1b8e83daffb5a0b6ec84763d145e9972c828
Ying Zhong <0x00eeee@gmail.com>
2025-10-18 21:57:56 +0800

    More sample code (#34)

---
+++ IntentifyMac/Resources/Extensions/Render Markdown.js
@@ -0,0 +1,32 @@
+async function load(src) {
+  return new Promise(resolve => {
+    if (document.querySelector(`script[src="${src}"]`)) {
+      return resolve();
+    }
+
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = true;
+    script.onload = resolve;
+    document.head.appendChild(script);
+  });
+}
+
+/**
+ * Render Markdown content as HTML.
+ *
+ * @image eye
+ */
+async function main(content) {
+  await load('https://cdn.jsdelivr.net/npm/marked@16.4.1/lib/marked.umd.js');
+  const html = marked.parse(content);
+
+  return await Intentify.renderUI(
+    `
+    <style>body { padding: 10px }</style>
+    <button onclick='Intentify.returnValue(${JSON.stringify(html)})'>Copy HTML</button><br>
+    ${html}
+    `,
+    { title: 'Markdown Preview', width: 640, height: 640 },
+  );
+}
---
+++ IntentifyMac/Resources/Extensions/Render Mermaid.js
@@ -0,0 +1,35 @@
+async function load(src) {
+  return new Promise(resolve => {
+    if (document.querySelector(`script[src="${src}"]`)) {
+      return resolve();
+    }
+
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = true;
+    script.onload = resolve;
+    document.head.appendChild(script);
+  });
+}
+
+/**
+ * Render Mermaid diagrams as HTML.
+ *
+ * @image fish
+ */
+async function main(content) {
+  await load('https://cdn.jsdelivr.net/npm/mermaid@11.12.0/dist/mermaid.min.js');
+  const isDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const theme = isDark ? 'dark' : 'default';
+  mermaid.initialize({ theme, startOnLoad: false });
+
+  const { svg: html } = await mermaid.render('graph', content);
+  return await Intentify.renderUI(
+    `
+    <style>body { padding: 10px }</style>
+    <button onclick='Intentify.returnValue(${JSON.stringify(html)})'>Copy HTML</button><br>
+    ${html}
+    `,
+    { title: 'Mermaid Preview', width: 640, height: 640 },
+  );
+}
